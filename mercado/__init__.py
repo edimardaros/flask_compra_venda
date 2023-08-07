286d9a65b9678bc72078ed380b37b2c7dede9b2b
Edimar Daros <edimar.daros@gmail.com>
2023-08-07 16:28:33 -0300

    Ajust DB Name

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado2.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
 db.init_app(app)
 
 from mercado import routes
