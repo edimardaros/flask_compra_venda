23f46293d93634ab37dcf5f4f102f197c3da6b18
Edimar Daros <edimar.daros@gmail.com>
2023-08-07 19:05:34 -0300

    Form cadastro

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = '7e8a41c9c6926159186204e3'
 db.init_app(app)
 
 from mercado import routes
